c38d021f553d8e995409659674438c3b959e7cec
Ashwin Santhanam <ashwinsanthanam@192.168.1.2>
2024-04-07 18:00:56 +0530

    make Space class aware of the SpatialData type

--- src/main/kotlin/core/point/SpatialData.kt
+++ src/main/kotlin/core/point/SpatialData.kt
@@ -4,13 +4,14 @@ interface SpatialData {
     val data: Array<Int>
 
     companion object {
-        fun spatialDataFactory(data: List<Int>): SpatialData {
+        @Suppress("UNCHECKED_CAST")
+        fun <T> spatialDataFactory(data: List<Int>): T where T : SpatialData {
             return when (data.size) {
                 1 -> SpatialData1D(x = data[0])
                 2 -> SpatialData2D(x = data[0], y = data[1])
                 3 -> SpatialData3D(x = data[0], y = data[1], z = data[2])
                 else -> throw NotImplementedError("SpatialData not implemented for ${data.size}")
-            }
+            } as T
         }
     }
 }
--- src/main/kotlin/core/space/Space.kt
+++ src/main/kotlin/core/space/Space.kt
@@ -2,15 +2,15 @@ package graphics.noise.perlin.core.space
 
 import graphics.noise.perlin.core.point.SpatialData
 
-data class Space(
-    val dimension: SpatialData,
-    val position: SpatialData
-) {
+data class Space<T> (
+    val dimension: T,
+    val position: T
+) where T : SpatialData {
     fun points(): List<SpatialData> {
         return points(0, emptyList())
     }
 
-    private fun points(axis: Int, coordinates: List<Int>): List<SpatialData> {
+    private fun points(axis: Int, coordinates: List<Int>): List<T> {
         if (axis >= dimension.data.size) return listOf(SpatialData.spatialDataFactory(coordinates))
         return (0 ..< dimension.data[axis]).flatMap {
             points(axis + 1, coordinates + (it + position.data[axis]))
