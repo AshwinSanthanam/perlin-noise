631fe20ecb8ee59d541b55974b16e89b8df15d4b
ashwinsanthanam <ashwinsanthanam098@gmail.com>
2024-04-13 12:20:40 +0530

    Space boundaries refactor

--- src/main/kotlin/core/Space.kt
+++ src/main/kotlin/core/Space.kt
@@ -20,20 +20,27 @@ data class Space<T> (
         }
     }
 
-    private fun boundaries(): List<T> = (0..<(1 shl size))
-        .map { Integer.toBinaryString(it) }
-        .map { binaryString -> binaryString
-            .map { it == '1' }
-            .leftPadding(size = size - binaryString.length)
+    private fun boundaries(): List<T> = rawBoundaries(size)
+        .map { rawBoundaries -> rawBoundaries
             .zip(dimension.data)
             .map {
                 if (it.first) it.second
                 else 0
             }
+        }
+        .map { boundaries -> boundaries
             .zip(position.data)
             .map { it.first + it.second }
         }
         .map { SpatialData.spatialDataFactory(it) }
 
+    private fun rawBoundaries(dimensions: Int) = (0..<(1 shl dimensions))
+        .map { Integer.toBinaryString(it) }
+        .map { binaryString -> binaryString
+            .map { it == '1' }
+            .leftPadding(size = dimensions - binaryString.length)
+        }
+
     private fun List<Boolean>.leftPadding(size: Int) = MutableList(size) { false }.plus(this)
+
 }
