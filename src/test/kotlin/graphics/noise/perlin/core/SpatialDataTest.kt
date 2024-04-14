24dc893e7b33710c7cc172d8a8cd1b0470f5868c
ashwinsanthanam <ashwinsanthanam098@gmail.com>
2024-04-14 14:48:21 +0530

    Refactor tests

--- src/test/kotlin/graphics/noise/perlin/core/SpatialDataTest.kt
+++ src/test/kotlin/graphics/noise/perlin/core/SpatialDataTest.kt
@@ -7,21 +7,5 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class SpatialDataTest {
-    @Test
-    fun `should return x value in array`() {
-        val spatialData1D: SpatialData = SpatialData1D(1)
-        assertEquals(listOf(1), spatialData1D.toList())
-    }
 
-    @Test
-    fun `should return the x, y values in an array`() {
-        val spatialData2D: SpatialData = SpatialData2D(1, 2)
-        assertEquals(listOf(1, 2), spatialData2D.toList())
-    }
-
-    @Test
-    fun `should return x, y, z value in array`() {
-        val spatialData3D: SpatialData = SpatialData3D(x = 1, y = 2, z = 3)
-        assertEquals(listOf(1, 2, 3), spatialData3D.toList())
-    }
 }
--- src/test/kotlin/graphics/noise/perlin/extension/spatial/data/SpatialDataExtensionTest.kt
+++ src/test/kotlin/graphics/noise/perlin/extension/spatial/data/SpatialDataExtensionTest.kt
@@ -1,9 +1,29 @@
 package graphics.noise.perlin.extension.spatial.data
 
+import graphics.noise.perlin.core.SpatialData
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class SpatialDataExtensionTest {
+
+    @Test
+    fun `should return x value in array`() {
+        val spatialData1D: SpatialData = SpatialData1D(1)
+        assertEquals(listOf(1), spatialData1D.toList())
+    }
+
+    @Test
+    fun `should return the x, y values in an array`() {
+        val spatialData2D: SpatialData = SpatialData2D(1, 2)
+        assertEquals(listOf(1, 2), spatialData2D.toList())
+    }
+
+    @Test
+    fun `should return x, y, z value in array`() {
+        val spatialData3D: SpatialData = SpatialData3D(x = 1, y = 2, z = 3)
+        assertEquals(listOf(1, 2, 3), spatialData3D.toList())
+    }
+
     @Test
     fun `should create new 1D spatial data`() {
         val spatialData = SpatialData1D(x = 1)
