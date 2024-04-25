68ce3786aa24c2dca608e3092648fe63739d149e
ashwinsanthanam <ashwinsanthanam098@gmail.com>
2024-04-25 23:56:10 +0530

    Add interpolator

--- src/main/kotlin/graphics/noise/perlin/core/Interpolator.kt
+++ src/main/kotlin/graphics/noise/perlin/core/Interpolator.kt
@@ -1,4 +1,21 @@
 package graphics.noise.perlin.core
 
-class Interpolator {
+class Interpolator<T : SpatialData> {
+
+    fun interpolate(position: T, boundaries: List<Coordinate>): Coordinate {
+        var currentBoundaryList = boundaries
+        for (coordinate in position) {
+            val newBoundaryList = mutableListOf<Coordinate>()
+            for (j in currentBoundaryList.indices step 2) {
+                newBoundaryList += interpolate(currentBoundaryList[j], currentBoundaryList[j + 1], coordinate)
+            }
+            currentBoundaryList = newBoundaryList
+        }
+        return currentBoundaryList[0]
+    }
+
+    private fun interpolate(lowPoint: Coordinate, highPoint: Coordinate, distanceFromLowPoint: Coordinate): Coordinate {
+        return lowPoint + distanceFromLowPoint * (highPoint - lowPoint)
+    }
+
 }
--- src/test/kotlin/graphics/noise/perlin/core/InterpolatorTest.kt
+++ src/test/kotlin/graphics/noise/perlin/core/InterpolatorTest.kt
@@ -1,5 +1,24 @@
 package graphics.noise.perlin.core
 
-import org.junit.jupiter.api.Assertions.*
+import graphics.noise.perlin.extension.spatial.data.SpatialData3D
+import io.kotlintest.shouldBe
+import org.junit.jupiter.api.Test
 
-class InterpolatorTest
+class InterpolatorTest {
+    @Test
+    fun `should interpolate 3D spatial data`() {
+        Interpolator<SpatialData3D>().interpolate(
+            position = SpatialData3D(x = Coordinate(0.5), y = Coordinate(0.2), z = Coordinate(0.7)),
+            boundaries = listOf(
+                Coordinate(0),
+                Coordinate(10),
+                Coordinate(20),
+                Coordinate(30),
+                Coordinate(50),
+                Coordinate(60),
+                Coordinate(70),
+                Coordinate(80)
+            )
+        ) shouldBe Coordinate(44)
+    }
+}
