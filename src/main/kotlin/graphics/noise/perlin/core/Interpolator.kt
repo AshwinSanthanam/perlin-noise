22099fe40c8dcac536ca7df5e0dc7ff04fec445d
ashwinsanthanam <ashwinsanthanam098@gmail.com>
2024-04-26 00:13:29 +0530

    Refactor

--- src/main/kotlin/graphics/noise/perlin/core/Interpolator.kt
+++ src/main/kotlin/graphics/noise/perlin/core/Interpolator.kt
@@ -2,20 +2,21 @@ package graphics.noise.perlin.core
 
 class Interpolator<T : SpatialData> {
 
-    fun interpolate(position: T, boundaries: List<Coordinate>): Coordinate {
-        var currentBoundaryList = boundaries
-        for (coordinate in position) {
-            val newBoundaryList = mutableListOf<Coordinate>()
-            for (j in currentBoundaryList.indices step 2) {
-                newBoundaryList += interpolate(currentBoundaryList[j], currentBoundaryList[j + 1], coordinate)
-            }
-            currentBoundaryList = newBoundaryList
-        }
-        return currentBoundaryList[0]
-    }
+    fun interpolate(position: T, boundaries: List<Coordinate>): Coordinate = interpolate(
+        position = position,
+        boundaries = boundaries,
+        index = 0
+    )
 
-    private fun interpolate(lowPoint: Coordinate, highPoint: Coordinate, distanceFromLowPoint: Coordinate): Coordinate {
-        return lowPoint + distanceFromLowPoint * (highPoint - lowPoint)
-    }
+    private fun interpolate(position: T, boundaries: List<Coordinate>, index: Int): Coordinate =
+        if (index >= position.size) boundaries[0]
+        else interpolate(
+            position = position,
+            boundaries = boundaries.chunked(2).map { interpolate(it[0], it[1], position[index]) },
+            index = index + 1
+        )
+
+    private fun interpolate(lowPoint: Coordinate, highPoint: Coordinate, distanceFromLowPoint: Coordinate): Coordinate =
+        lowPoint + distanceFromLowPoint * (highPoint - lowPoint)
 
 }
